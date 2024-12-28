f4b3c4cd4a7aa20481bf520d4d5a0e629bc38031
Janell Huyck <Janell.Huyck@outlook.com>
2024-12-27 22:58:08 -0500

    wip: allow commit type 'wip'

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,23 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2, // Error level: 2 = error
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'wip', // Add 'wip' as a valid type
+      ],
+    ],
+  },
 };
